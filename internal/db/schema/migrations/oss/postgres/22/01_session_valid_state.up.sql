78b687394264beaecdaa38698c2b851f3cac5bd5
Irena Rindos <irena.rindos@gmail.com>
2021-12-21 13:34:41 -0500

    fixup! bug(session): Allow only valid state transitions

--- internal/db/schema/migrations/oss/postgres/22/01_session_valid_state.up.sql
+++ internal/db/schema/migrations/oss/postgres/22/01_session_valid_state.up.sql
@@ -1,4 +1,40 @@
 begin;
+
+-- Remove invalid (states with no prior end time) rows
+delete from session_state where previous_end_time is null and state != 'pending';
+
+-- In the case of dupes that are otherwise valid, identify first valid session
+create temp table session_state_delete
+as select session_id, min(start_time) as st_t from session_state ou
+   where (select count(*) from session_state inr
+          where inr.session_id = ou.session_id and inr.state = ou.state) > 1
+   group by session_id;
+
+-- Remove all session rows after first valid session
+DELETE FROM session_state t1
+    using session_state_delete t2 where t1.session_id =t2.session_id and t1.start_time > t2.st_t;
+
+drop table session_state_delete;
+
+-- Update session_state so any active/ pending sessions are set to canceling
+-- This will cancel sessions when we close all open session connections
+insert into session_state (session_id, state)
+    (select s1.session_id, 'canceling' from session_state s1
+        inner join (
+            select session_id, max(start_time) as st from session_state
+            group by session_id) s2
+        on s1.session_id=s2.session_id and s1.start_time=s2.st
+     where state in('pending','active'));
+
+-- Close all open session_connections
+-- This will trigger the closure of sessions: update_connection_state_on_closed_reason -> terminate_session_if_possible
+update session_connection
+set closed_reason='canceled' where public_id in
+    (select public_id from session_connection where closed_reason is null);
+
+-- Migration
+
+-- session_valid_state table creation and related constraints
 create table session_valid_state(
     prior_state text
         references session_state_enm(name)
@@ -62,4 +98,5 @@ $$ language plpgsql;
 
 create trigger update_session_state before insert on session_state
     for each row execute procedure update_prior_session_state();
+
 commit;
